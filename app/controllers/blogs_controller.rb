5ed0edefc3b5f9b261532a4f6357d30c250aee38
kazutotaniyoshi <it352032@gmail.com>
2021-10-05 14:29:20 +0000

    test

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,20 +1,21 @@
 class BlogsController < ApplicationController
   def index
+      @blogs = Blog.all
   end
 
   def show
-    @blog = Blog.new
-     blog.save
-    redirect_to blogs_path
   end
 
   def new
+     @blog = Blog.new
+     blog.save
+    redirect_to blogs_path
   end
-
+  
   def edit
   end
 
-  privete
+  private
   def blog_params
    params.require(:blog).permit(:title, :category, :body)
   end
